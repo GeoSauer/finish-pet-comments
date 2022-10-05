50035f5142a4c9252b0b94dfe27d053d7e17e47a
GeoSauer <geoffrey.sauer89@gmail.com>
2022-10-05 13:04:33 -0600

    add pet items link to pet detail page

--- render-utils.js
+++ render-utils.js
@@ -3,8 +3,8 @@ export function renderPet(pet) {
 
     // Part A: add an anchor link tag
     //      - set the link to be like `/pet/?id=34`, but use the actual pet id
-    //      - adjust the content to be in the anchor link,
-    //        and the anchor link is appended to the li
+    const a = document.createElement('a');
+    a.href = `/pet/?id=${pet.id}`;
 
     const img = document.createElement('img');
     img.src = pet.image_url;
@@ -14,8 +14,10 @@ export function renderPet(pet) {
 
     const p = document.createElement('p');
     p.textContent = pet.bio;
-
-    li.append(img, h2, p);
+    //      - adjust the content to be in the anchor link,
+    //        and the anchor link is appended to the li
+    a.append(img, h2, p);
+    li.append(a);
 
     return li;
 }
